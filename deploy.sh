921dce74bb845740db1de105d7f9524c4f6277b5
José M. Camacho <hello@josemazo.com>
2017-07-04 22:06:50 +0200

    Reducing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker run --rm --name='que-pasa-perri' -v $DIR:/usr/src/app node:7.3.0 bash -c 'cd /usr/src/app && exec node server/app.js' >&1
 
